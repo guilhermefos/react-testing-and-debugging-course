fe0cfca8e027e599775a5bb96ceef98b05cad2b5
Guilherme Oliveira <guilhermefos@users.noreply.github.com>
2020-12-30 19:36:10 -0300

    Wrapping test with describe

---
+++ src/components/listings/Grid.spec.js
@@ -0,0 +1,46 @@
+import data from "../../data/courses.json";
+
+const numItems = data.length;
+
+describe("Number test", () => {
+  test("Number of items = 12", () => {
+    expect(numItems).toBe(12);
+  });
+
+  test("Number of items to be greater than or equal to 12", () => {
+    expect(numItems).toBeGreaterThanOrEqual(12);
+  });
+});
+
+const title = data[0].title;
+
+describe("Title test", () => {
+  test("There is a JS in this title", () => {
+    expect(title).toMatch(/JS/);
+  });
+
+  test("The title contain React", () => {
+    expect(title).toContain("React");
+  });
+});
+
+// Arrays
+const data2 = ["React Native", "React"];
+
+describe("Array test", () => {
+  test("The list of courses contains React Native and React", () => {
+    expect(["React Native", "React", "MeteorJS"]).toEqual(
+      expect.arrayContaining(data2)
+    );
+  });
+});
+
+describe("Object test", () => {
+  test("The first course to have a property title", () => {
+    expect(data[0]).toHaveProperty("title");
+  });
+
+  test("The first course to have a property title and value", () => {
+    expect(data[0]).toHaveProperty("views", "31,266");
+  });
+});
